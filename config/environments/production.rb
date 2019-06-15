8a99dbd172ff8b3b2c89791d78142209632a08f4
Brady Riordan <bradyriordan@hotmail.com>
2019-06-15 07:31:58 -0400

    Configure sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,13 +99,13 @@ Rails.application.configure do
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = { 
-      address:"smtp.gmail.com",
-      port:587,
-      domain:"gmail.com",
-      authentication: "plain",
-      user_name:"bradyclarkriordan@gmail.com",
-      password:"$piderman1987",
-      enable_starttls_auto: true 
+      :user_name => 'app136285734@heroku.com',
+      :password => 'j1d11rvg3525',
+      :domain => 'greenbeta.ca',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
     }
 
   # Do not dump schema after migrations.
