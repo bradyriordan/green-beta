a2ceebd932f856d6ff5e7b65daf4cb8f874aea33
Brady Riordan <bradyriordan@hotmail.com>
2019-06-25 16:34:50 -0400

    Implement daily carbon beta

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,9 +2,10 @@ class PublicController < ApplicationController
   
   layout 'home'
 
-  def home
-    @companies = Company.count
+  def home    
     @contact = Contact.new
+    @company = Rails.cache.fetch( "term", :expires_in => 1.day){ Company.offset(rand(Company.count)).first }    
+    @company_data = CompanyYearlyDatum.most_recent(@company.id)
   end
 
   def contact    
